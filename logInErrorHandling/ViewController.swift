a0faeb3137f20d4da89ec0a03af4be501f96a2f0
diliptilonia <diliptilonia@gmail.com>
2017-09-16 21:00:55 +0530

    Initial Commit

---
+++ logInErrorHandling/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  logInErrorHandling
+//
+//  Created by francois buisson on 16/09/17.
+//  Copyright © 2017 dilipGurjar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
